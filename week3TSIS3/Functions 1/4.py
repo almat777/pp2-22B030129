abb1af38f66729ae040145e7732aeec59091aa0c
almaty <almat_abdimalik@mail.ru>
2023-02-11 18:26:32 +0600

    zoro

--- week3TSIS3/Functions 1/4.py
+++ week3TSIS3/Functions 1/4.py
@@ -1,8 +1,12 @@
-def print(n):
-    for i in range(2,n):
+def filter_prime(n):
+    for i in range(2, n):
         if n % i == 0:
             return False
-        return True
+    return True
 n = int(input())
 l = list()
 for i in range(n):
+    x = int(input())
+    if filter_prime(x):
+        l.append(x)
+print(l)
