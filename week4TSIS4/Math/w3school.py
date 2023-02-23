82ca16e816ee6ca26407ed58aa8e6599b6088b67
almaty <almat_abdimalik@mail.ru>
2023-02-23 22:14:13 +0600

    zoro

---
+++ week4TSIS4/Math/w3school.py
@@ -0,0 +1,34 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+print()
+
+x = abs(-7.25)
+
+print(x)
+print()
+
+x = pow(4, 3)
+
+print(x)
+print()
+
+import math
+
+x = math.sqrt(64)
+
+print(x)
+print()
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)
+print(y)
+print()
+
+x = math.pi
+
+print(x)
