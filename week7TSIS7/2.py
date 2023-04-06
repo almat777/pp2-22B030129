bed6182717dc52d92d6c4b2f83587414f62d67c5
almaty <almat_abdimalik@mail.ru>
2023-04-06 21:55:34 +0600

    zoro

--- week7TSIS7/2.py
+++ week7TSIS7/2.py
@@ -2,15 +2,12 @@ import pygame.mixer
 import pygame.time
 import keyboard
 
-# Initialize mixer
 pygame.mixer.init()
 
-# Load music files
-playlist = ["mockinbird.mp3",]
+playlist = ["music/mockinbird.mp3", ]
 current_track = 0
 pygame.mixer.music.load(playlist[current_track])
 
-# Define keyboard control functions
 def play():
     pygame.mixer.music.play()
 
@@ -28,13 +25,10 @@ def prev_track():
     current_track = (current_track - 1) % len(playlist)
     pygame.mixer.music.load(playlist[current_track])
     pygame.mixer.music.play()
-
-# Map keyboard keys to control functions
 keyboard.add_hotkey('space', play)
 keyboard.add_hotkey('esc', stop)
 keyboard.add_hotkey('right', next_track)
 keyboard.add_hotkey('left', prev_track)
 
-# Start event loop
 while True:
     pygame.time.wait(100)
